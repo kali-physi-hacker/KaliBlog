a8857307480e21242c1a95fabf7b12c72b4c6fe7
Desmond Brown <browndesmond30@yahoo.com>
2020-06-24 23:26:37 +0000

    Kali: Database Configuration for Deployment to Github Actions

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'github_actions',
+        'USER': 'radorado',
+        'PASSWORD': 'radorado',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
