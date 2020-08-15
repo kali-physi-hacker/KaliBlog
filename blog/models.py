c63429ab393fc626b298bb3fac098fd858583563
Desmond Brown <browndesmond30@yahoo.com>
2020-08-15 13:31:20 +0000

    Kali: Fixed Comment model __str__ method bug

--- blog/models.py
+++ blog/models.py
@@ -130,8 +130,6 @@ class Post(models.Model):
 class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post_comments")
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    # name = models.CharField(max_length=80)
-    # email = models.EmailField()
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -141,4 +139,4 @@ class Comment(models.Model):
         ordering = ('created', )
 
     def __str__(self):
-        return f'Comment by {self.name} on {self.post}'
+        return f'Comment by {self.author.full_name()} on {self.post}'
