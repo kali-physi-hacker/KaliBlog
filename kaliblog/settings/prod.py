0a7e15290333aad770cfd83d8764032cdb2d7ee2
Desmond Brown <browndesmond30@yahoo.com>
2020-08-20 18:58:29 +0000

    Kali: Initialize Settings files

--- kaliblog/settings/prod.py
+++ kaliblog/settings/prod.py
@@ -1,20 +1,16 @@
-from kaliblog.settings import *
+from kaliblog.settings.base import *
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'kaliblog.herokuapp.com']
 
-DEBUG = False
+SECRET_KEY = os.environ.get('SECRET_KEY', 'not-so-secret-key')
 
-#
-# DATABASES = {
-#     "default": {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'webmaster',
-#         'USER': 'webmaster',
-#         'PASSWORD': 'admin',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
+DEBUG = int(os.environ.get('DEBUG', default=1))
+
+# DATABASE CONFIGURATION
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
 
 # EMAIL CONFIGURATION FOR GMAIL SMTP
 EMAIL_HOST = 'smtp.gmail.com'
@@ -22,3 +18,5 @@ EMAIL_HOST_USER = 'brownjunior956@gmail.com'
 EMAIL_HOST_PASSWORD = 'BLACKFOXA1B2C3IQ'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
